7868ab0a9f4ceb112fe1c80ce828485a9d532787
Peter Nagy <petnagy@gmail.com>
2016-06-22 23:01:48 +0200

    Save Visitor into datastore with objectify

---
+++ appengine_workshop/src/com/example/app/Visitor.java
@@ -0,0 +1,60 @@
+package com.example.app;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+@Entity
+public class Visitor {
+
+	@Id
+	private Long id;
+	
+	private String nickName;
+	
+	private String email;
+	
+	private Long visitTimeStamp;
+	
+	private String visitTime;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getVisitTimeStamp() {
+		return visitTimeStamp;
+	}
+
+	public void setVisitTimeStamp(Long visitTimeStamp) {
+		this.visitTimeStamp = visitTimeStamp;
+	}
+
+	public String getVisitTime() {
+		return visitTime;
+	}
+
+	public void setVisitTime(String visitTime) {
+		this.visitTime = visitTime;
+	}
+	
+}
--- appengine_workshop/src/com/example/app/VisitorServlet.java
+++ appengine_workshop/src/com/example/app/VisitorServlet.java
@@ -1,6 +1,11 @@
 package com.example.app;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+import java.util.TimeZone;
 import java.util.logging.Logger;
 
 import javax.servlet.http.*;
@@ -8,11 +13,15 @@ import javax.servlet.http.*;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import com.googlecode.objectify.ObjectifyService;
 
 @SuppressWarnings("serial")
 public class VisitorServlet extends HttpServlet {
 	
 	private static final Logger log = Logger.getLogger(VisitorServlet.class.getName());
+
+	private static final String CET = "Europe/Budapest";
+	private static final String DATE_TIME_FORMAT = "yyyy.MM.dd. HH:mm.ss";
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		UserService userService = UserServiceFactory.getUserService();
@@ -21,19 +30,63 @@ public class VisitorServlet extends HttpServlet {
 		String thisUrl = req.getRequestURI();
 		String result = "<p>Welcome ";
 		String userName;
+		Visitor visitor;
 		if (user != null) {
 			//User logged in
 			log.info("User INFO: " + user.toString());
 			userName = user.getNickname();
 			result += userName + "</p> \n";
 			result += "<p>You can <a href=\"" + userService.createLogoutURL(thisUrl) + "\">sign out</a>.</p>";
+			visitor = createVisitor(user);
 		} else {
 			//Anonim user
 			userName = "Anonimous";
 			result += userName + "</p> \n";
 			result += "<p>You can <a href=\"" + userService.createLoginURL(thisUrl) + "\">sign in</a>.</p>";
+			visitor = createAnonimous();
 		}
+		
+		ObjectifyService.ofy().cache(false).save().entity(visitor).now();
+		
+		result += "<br>Visitors: <br>";
+		List<Visitor> visitors = ObjectifyService.ofy().cache(false).load().type(Visitor.class).list();
+		for(Visitor oldVisitor : visitors) {
+			result += oldVisitor.getVisitTime() + " " + oldVisitor.getNickName() + " " + oldVisitor.getEmail() + "<br>";
+		}
+		
 		resp.getWriter().println(result);
 		resp.setContentType("text/html");
+	}
+	
+	private Visitor createVisitor(User user) {
+		Visitor visitor = new Visitor();
+		visitor.setEmail(user.getEmail());
+		visitor.setNickName(user.getNickname());
+		long nowTimeStamp = getDateNow();
+		visitor.setVisitTime(getHungarianDateTime(nowTimeStamp));
+		visitor.setVisitTimeStamp(nowTimeStamp);
+		return visitor;
+	}
+	
+	private Visitor createAnonimous() {
+		Visitor visitor = new Visitor();
+		visitor.setEmail("");
+		visitor.setNickName("Anonimous");
+		long nowTimeStamp = getDateNow();
+		visitor.setVisitTime(getHungarianDateTime(nowTimeStamp));
+		visitor.setVisitTimeStamp(nowTimeStamp);
+		return visitor;
+	}
+	
+	private long getDateNow() {
+		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone(CET));
+		return cal.getTimeInMillis();
+	}
+	
+	public String getHungarianDateTime(long timeStamp) {
+		Date date = new Date(timeStamp);
+		SimpleDateFormat formatter = new SimpleDateFormat(DATE_TIME_FORMAT);
+		formatter.setTimeZone(TimeZone.getTimeZone(CET));
+		return formatter.format(date);
 	}
 }
